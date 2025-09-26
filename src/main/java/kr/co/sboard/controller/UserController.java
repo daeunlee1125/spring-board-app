70a297bd66cf2df8ff70bc4bbb37aab280ebf4de
daeunlee1125 <daeunlee1125@gmail.com>
2025-09-26 12:21:11 +0900

    add 0.0.1

--- src/main/java/kr/co/sboard/controller/UserController.java
+++ src/main/java/kr/co/sboard/controller/UserController.java
@@ -1,8 +1,11 @@
 package kr.co.sboard.controller;
 
+import kr.co.sboard.config.AppInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,23 +13,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class UserController {
 
+
+    private final AppInfo appInfo;
+
     @GetMapping("/user/info")
-    public String info(){
+    public String info() {
+
         return "user/info";
     }
 
     @GetMapping("/user/login")
     public String login(){
+
         return "user/login";
     }
 
     @GetMapping("/user/register")
     public String register(){
+
         return "user/register";
     }
 
     @GetMapping("/user/terms")
     public String terms(){
+
         return "user/terms";
     }
 }
